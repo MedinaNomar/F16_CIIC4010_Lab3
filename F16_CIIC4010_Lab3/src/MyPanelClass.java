338c5e20f8b940b2b3b1d0ce4ac4d8b71eb9d6a6
4010a3 <4010a3@guineo.ece.uprm.edu>
2016-08-31 09:27:24 -0400

    Added the code as instructed

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -22,15 +22,26 @@ public class MyPanelClass extends JPanel {
                         int height = y2 - y1;
  
                         //Paint the background
-                        g.setColor(Color.GRAY);
+                        g.setColor(Color.blue);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-                        //Draw a border
+                          //Draw a border
                         g.setColor(Color.YELLOW);
                         g.drawRect(x1, y1, width, height);
                         
                         //Draw an inner border
                         g.setColor(Color.red);
                         g.drawRect(x1 + 5, y1 + 5, width - 11, height - 11);
+                        
+                        //Diagonal line
+                        g.setColor(Color.WHITE);
+                        g.drawLine(x1, y1, x2, y2);
+                        
+                        g.setColor(Color.WHITE);
+                        g.drawLine(x1, y1 + width, x2, y2 - width);
+                        
+                        //Another diagonal line
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.drawOval(x1, y1, 55, 55);
             }
 }
