ceb1925de7d53ba1a7a078f97eb13b802a524ca1
potatoemaybe <potatoemaybe@potatoemaybe-VirtualBox>
2016-08-31 21:01:09 -0400

    Added code for question 7

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -26,23 +28,46 @@ public class MyPanelClass extends JPanel {
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
                         //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width, height);
-                        
-                        //Draw an inner border
-                        g.setColor(Color.red);
-                        g.drawRect(x1 + 5, y1 + 5, width - 11, height - 11);
-                        
-                        //Diagonal line
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        //Draw an inner border
+//                        g.setColor(Color.red);
+//                        g.drawRect(x1 + 5, y1 + 5, width - 11, height - 11);
+//                        
+//                        //Diagonal line
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        //Another one: diagonal line
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1 + width, x2, y2 - width);
+//                        
+//                        //And another one (oval)
+//                        g.setColor(Color.LIGHT_GRAY);
+//                        g.fillOval((width - 55)/2 , (height - 55)/2, 55, 55);
+//                        Polygon p = new Polygon();
+//                        p.addPoint(x1 + 5, y1 + 25);
+//                        p.addPoint(x1 + 20, y1 + 10);
+//                        p.addPoint(x1 + 35, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 25);
+//                        g.setColor(Color.YELLOW);
+//                        g.fillPolygon(p);
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
                         g.setColor(Color.WHITE);
-                        g.drawLine(x1, y1, x2, y2);
-                        
-                        //Another one: diagonal line
-                        g.setColor(Color.WHITE);
-                        g.drawLine(x1, y1 + width, x2, y2 - width);
-                        
-                        //And another one (oval)
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval((width - 55)/2 , (height - 55)/2, 55, 55);
+                        g.drawPolygon(p2);
             }
 }
